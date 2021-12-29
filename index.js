eed01e3d61c23be56e98e1696f7c56ad50504da8
jhon-program-nodejs <jonathan.programnodejs@gmail.com>
2021-12-28 22:42:01 -0300

    Bot login

---
+++ index.js
@@ -0,0 +1,68 @@
+const puppeteer = require('puppeteer');
+
+
+async function getBunnies() {
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: "C:/Program Files/Google/Chrome Dev/Application/chrome",
+  });
+  const page = await browser.newPage();
+
+  const returnError = async () => {
+    await page.waitForTimeout(6000);
+
+    await page.type("#identifierId", 'jonathanrepresentacoesja@gmail.com')
+
+    await page.waitForTimeout(3000);
+
+    await page.click("#identifierNext > div > button > span")
+
+    await page.waitForTimeout(1000);
+
+  }
+
+  const buttonReturn = async () => {
+    await page.evaluate(() => {
+      const buuton = document.querySelector('[jsname="V67aGc"]')
+
+
+
+      if (buuton.tagName === "SPAN") {
+
+        setTimeout(async () => {
+          await buuton.click()
+          await page.waitForTimeout(5000)
+          await returnError()
+        }, 3000)
+      }
+      return buuton
+
+
+    })
+
+  }
+
+  const url = 'https://google.com'
+
+  await page.goto(url);
+
+  await page.waitForTimeout(2000);
+
+  await page.click('#gb > div > div.gb_Me > a')
+
+  await returnError()
+
+  await buttonReturn()
+  
+  await page.waitForTimeout(7000);
+  
+  await browser.close()
+}
+
+getBunnies()
+
+setInterval(async () => {
+getBunnies()
+}, 30000)
+
